50fcaf6d6a15f1918b1b1fd6b5aeb38293d229da
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-11 18:01:14 -0400

    commit tree 7c31fab4eafa7bc216b6435e40962501cbcac2db

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+  static titleize(string) {
+    let prep = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    string = string.split(" ") 
+    string = string.map((word, index) => {
+      if (index === 0) {
+        word = this.capitalize(word)
+        return word
+      } else if (!prep.includes(word)) {
+        word = this.capitalize(word)
+        return word
+      } else {
+        return word
+      }
+    })
+    return string.join(" ")
+  }
 }
+// } for (word.charAt(0)){
+//   return this.capitalize(word)
+// } else {
